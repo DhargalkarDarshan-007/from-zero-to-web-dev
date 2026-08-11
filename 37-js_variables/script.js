bf6af8db3ad414e8f47b40719a004dac26d20cff
Darshan Dhargalkar <darshandharaglkar@gmail.com>
2026-08-11 20:11:42 +0530

    Add initial HTML and JavaScript files for tutorial on variables and data types

---
+++ 37-js_variables/script.js
@@ -0,0 +1,29 @@
+
+// ==========================================
+// JavaScript Tutorial 55 - Variables & Data Types
+// ==========================================
+
+console.log("Hey, this is Tutorial 55");
+
+// ------------------------------------------
+// 1. Variables using var, let and const
+// ------------------------------------------
+
+var a = 5;
+// a = a + 1;  // We can change the value of a
+
+let b = 6;
+let c = "Harry";
+let _a = "Shubham";
+
+// Variable names cannot start with a number
+// var 55a = "Rohan"; // ❌ Not allowed
+
+
+// ------------------------------------------
+// 2. Checking variable values and types
+// ------------------------------------------
+
+console.log(a + b + 8);
+console.log(typeof a, typeof b, typeof c);
+
