30b5846da6b2794b3a85443341c9b61ebb960156
Darshan Dhargalkar <darshandharaglkar@gmail.com>
2026-08-14 12:45:58 +0530

    Add initial implementation of array examples in JavaScript

---
+++ 44-arrays/index.js
@@ -0,0 +1,13 @@
+let arr = [4, 5, 6, 3, 2, 1]
+let arr1 = ["ram", "sham"]
+let arr2 = [0, 5, 8]
+console.log(arr)
+console.log(arr.length)
+console.log(arr.sort())
+arr.pop()
+arr.push(6)
+arr.shift()
+arr.unshift(1)
+console.log(arr.concat(arr1, arr2))
+// console.log(arr.reverse())
+console.log(arr)
