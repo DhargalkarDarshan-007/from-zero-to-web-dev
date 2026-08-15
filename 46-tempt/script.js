d5769eb49d4965acaaae8a4f326e88500a9a362b
Darshan Dhargalkar <darshandharaglkar@gmail.com>
2026-08-15 11:13:46 +0530

    Print arr using Array.from(Array(number).keys()

---
+++ 46-tempt/script.js
@@ -0,0 +1,13 @@
+//  let a=7
+// function factorial(number){
+//    let arr = Array.from(Array(number+1).keys())  //arr start from 1 to 6
+//    let c = arr.slice(1,).reduce((a, b)=>{ 
+//     return a*b 
+// })
+//    return c
+// }
+
+let a=10
+let arr=Array.from(Array(a+1).keys())
+let c=arr.slice(1,)
+console.log(c)
