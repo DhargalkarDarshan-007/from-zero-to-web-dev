25f4559839002d95a31d932f91adff09f6df2a47
Darshan Dhargalkar <darshandharaglkar@gmail.com>
2026-08-15 12:17:50 +0530

    Add initial HTML structure and JavaScript for DOM manipulation examples

---
+++ 48-dom_nodes/script.js
@@ -0,0 +1,13 @@
+// console.log("Hello world")
+
+document.body.firstElementChild 
+document.body.firstElementChild.childNodes 
+document.body.firstElementChild.children
+
+console.log(document.body.children)
+console.log(document.body.children[0])
+console.log(document.body.children[0].children)
+console.log(document.body.children[0].children[1])
+console.log(document.body.children[0].children[1].nextElementSibling)
+console.log(document.body.children[0].children[1].previousElementSibling)
+console.log(document.body.children[0].children[1].previousElementSibling.parentNode)
