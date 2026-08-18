9253d76665c95da1d808dbb3b557886be42380ae
Darshan Dhargalkar <darshandharaglkar@gmail.com>
2026-08-18 21:12:51 +0530

    Refactor script loading with callback function and improve formatting

--- 55-callback&promices/scrip.js
+++ 55-callback&promices/scrip.js
@@ -2,13 +2,28 @@ console.log('this is promise');
 
 setTimeout(() => {
     console.log('I am inside settimeout');
-    
+
 }, 2000);
 
 setTimeout(() => {
     console.log('I am inside settimeout2');
-    
+
 }, 2000);
+
 console.log('the end');
+
+const callback = (arg) => {
+    console.log(arg)
+
+}
+
+const loadscript = (src, callback) => {
+    let sc = document.createElement("script");
+    sc.src = src;
+    sc.onload = callback("Harry");
+    document.head.append(sc);
+}
+
+loadscript("https://cdnjs.cloudflare.com/ajax/libs/prism/9000.0.1/prism.min.js", callback )
 
 
