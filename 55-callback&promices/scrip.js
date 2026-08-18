73cdb0a713d6df7effc9223b84a33dc9c205a50f
Darshan Dhargalkar <darshandharaglkar@gmail.com>
2026-08-18 20:35:46 +0530

    JavaScript files for callback and promises example

---
+++ 55-callback&promices/scrip.js
@@ -0,0 +1,14 @@
+console.log('this is promise');
+
+setTimeout(() => {
+    console.log('I am inside settimeout');
+    
+}, 2000);
+
+setTimeout(() => {
+    console.log('I am inside settimeout2');
+    
+}, 2000);
+console.log('the end');
+
+
