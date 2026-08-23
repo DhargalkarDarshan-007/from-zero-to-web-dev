aae352bfd322cb1c6e8cd1aa195320ab185a5e57
Darshan Dhargalkar <darshandharaglkar@gmail.com>
2026-08-23 18:26:01 +0530

    Refactor getData function to use promises and remove callback hell

--- 61-apnaclg/script.js
+++ 61-apnaclg/script.js
@@ -1,23 +1,51 @@
-function getData(dataId, getNextData) {
+// function getData(dataId, getNextData) {
+//         setTimeout(() => {
+//             console.log('data', dataId);
+//             if(getNextData){
+//                 getNextData();
+//             }
+
+//         }, 3000);
+
+// }
+// //callback hell
+// getData(1, () => {
+//     console.log('getting data2...');
+//     getData(2, () => {
+//         console.log('getting data3...');
+//         getData(3, () => {
+//             console.log('getting data4...');
+//             getData(4)
+//         })
+
+//     })
+
+// })
+
+
+
+
+
+function getData(dataId) {
+    return new Promise((resolve, reject) => {
         setTimeout(() => {
             console.log('data', dataId);
-            if(getNextData){
-                getNextData();
-            }
+            resolve("success")
 
         }, 3000);
-    
+    })
 }
-//callback hell
-getData(1, () => {
-    console.log('getting data2...');
-    getData(2, () => {
-        console.log('getting data3...');
-        getData(3, () => {
-            console.log('getting data4...');
-            getData(4)
+
+
+//promise chain
+getData(1).then((res) => {
+    console.log(res);
+    getData(2).then((res) => {
+        console.log(res);
+        getData(3).then((res) => {
+            console.log(res);
         })
 
     })
-
 })
+
